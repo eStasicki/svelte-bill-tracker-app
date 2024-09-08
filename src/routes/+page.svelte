75b9e70b79b99cca8ec51945bc44c970dbc7c12e
Ernest Stasicki <ernest.stasicki@monogo.pl>
2024-09-08 12:09:55 +0200

    init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<p class="p-8">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nostrum mollitia aliquam culpa
+		accusantium aut dolore odio eum. Iste quae deleniti explicabo alias voluptatum rerum ipsum
+		repellat sed minima labore.
+	</p>
+</section>
